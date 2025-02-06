59cb55af48a8b5d081d8f342748b0ee4132ad879
bmeinert8 <brian.meinert1@gmail.com>
2025-02-06 10:29:26 -0500

    refactor card dealing and display logic; enhance DOM updates and event handling

--- main.js
+++ main.js
@@ -5,64 +5,50 @@ const playerHand = [];
 const dealerHand = [];
 let resultHTML = '';
 
-//function to create a single deck of cards
+// Function to create a single deck of cards
 function buildDeck() {
   const deck = [];
-  for(const value of values) {
-    for(const suit of suits) {
+  for (const value of values) {
+    for (const suit of suits) {
       deck.push(`${value}-${suit}`);
     }
   }
   return deck;
 }
 
-//function to shuffle the deck of cards
+// Function to shuffle the deck of cards
 function shuffleDeck(deck) {
-  for(let i = deck.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i);
-    const temp = deck[i];
-    deck[i] = deck[j];
-    deck[j] = temp;
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
   }
-  return(deck);
-};
-
-shuffleDeck(buildDeck());
-
-//function to deal cards to the player and dealer
-function dealCards(deck, playerHand, dealerHand){
-  playerHand.push(deck.pop());
-  dealerHand.push(deck.pop());
-  playerHand.push(deck.pop());
-  dealerHand.push(deck.pop());
-  console.log(playerHand[0]);
-  console.log(dealerHand[0]);
-  console.log(playerHand[1]);
-  return playerHand, dealerHand;
+  return deck;
 }
 
-dealCards(deck, playerHand, dealerHand);
+// Function to deal cards to the player and dealer
+function dealCards(deck, playerHand, dealerHand) {
+  for (let i = 0; i < 2; i++) {
+    playerHand.push(deck.pop());
+    dealerHand.push(deck.pop());
+  }
+  updateDOM();
+}
 
-//display player cards on the DOM
-let card1 = playerHand[0];
-let card2 = playerHand[1];
+// Function to update the DOM with player and dealer cards
+function updateDOM() {
+  document.querySelector('.js-player-cards').innerHTML = playerHand.map(card => `
+    <img class="playing-card" src="./images/cards/${card}.png" alt="Image of a playing card">
+  `).join('');
 
-document.querySelector('.js-player-cards')
-  .innerHTML = `
-    <img class="playing-card" src="./images/cards/${card1}.png" alt="Image of a playing card">
-    <img class="playing-card" src="./images/cards/${card2}.png" alt="Image of a playing card">
-`;
+  document.querySelector('.js-dealer-cards').innerHTML = `
+    <img class="playing-card card-back" src="./images/cards/BACK.png" alt="Image of a playing card face down">
+    <img class="playing-card" src="./images/cards/${dealerHand[0]}.png" alt="Image of a playing card face up">
+  `;
 
-//display dealer cards on the DOM
-let dealerCard1 = dealerHand[0];
+  document.querySelector('.js-player-score').innerHTML = calculateHandTotal(playerHand);
+}
 
-document.querySelector('.js-dealer-cards')
-  .innerHTML = `
-     <img class="playing-card card-back" src="./images/cards/BACK.png" alt="Image of a playing card face down">
-      <img class="playing-card" src="./images/cards/${dealerCard1}.png" alt="Image of a playing card face up">
-  `
-
-//function to calculate the total value of the player and dealers hands
+// Function to calculate the total value of a hand
 function calculateHandTotal(hand) {
   let total = 0;
   let aces = 0;
@@ -83,19 +69,13 @@ function calculateHandTotal(hand) {
     total -= 10;
     aces -= 1;
   }
-  return(total);
+  return total;
 }
-calculateHandTotal(playerHand);
 
-//display the total value of the player hand on the DOM
-let displayTotal = calculateHandTotal(playerHand);
-
-document.querySelector('.js-player-score')
-  .innerHTML = `${displayTotal}`;
-
-//function to handle the hit button
-function hit(deck, playerHand){
+// Function to handle the hit button
+function hit(deck, playerHand) {
   playerHand.push(deck.pop());
+  updateDOM();
   const total = calculateHandTotal(playerHand);
   if (total > 21) {
     document.querySelector('.js-result').innerHTML = 'Player Bust';
@@ -103,81 +83,50 @@ function hit(deck, playerHand){
   }
 }
 
-//update the DOM with the new card and hand total when the player hits
-document.querySelector('.js-hit-button')
-  .addEventListener('click', function(){
-    hit(deck, playerHand);
-    let newCard = playerHand[playerHand.length - 1];
-    document.querySelector('.js-player-cards')
-      .innerHTML += `
-        <img class="playing-card" src="./images/cards/${newCard}.png" alt="Image of a playing card">
-      `;
-    let newTotal = calculateHandTotal(playerHand);
-    document.querySelector('.js-player-score')
-      .innerHTML = `${newTotal}`;
-  });
+// Function to handle the stand button and start the dealer's turn
+function stand(deck, dealerHand) {
+  document.querySelector('.js-dealer-cards').innerHTML = dealerHand.map(card => `
+    <img class="playing-card" src="./images/cards/${card}.png" alt="Image of a playing card">
+  `).join('');
 
+  document.querySelector('.js-dealer-score').innerHTML = calculateHandTotal(dealerHand);
 
-  //function to handle the stand button and start the dealer's turn
-function stand(deck, dealerHand){
-  document.querySelector('.js-dealer-cards')
-    .innerHTML = `
-      <img class="playing-card" src="./images/cards/${dealerHand[0]}.png" alt="Image of a playing card">
-      <img class="playing-card" src="./images/cards/${dealerHand[1]}.png" alt="Image of a playing card">
-    `; 
-  document.querySelector('.js-dealer-score')
-  .innerHTML = `${calculateHandTotal(dealerHand)}`;
   while (calculateHandTotal(dealerHand) < 17) {
     dealerHand.push(deck.pop());
-    document.querySelector('.js-dealer-cards')
-      .innerHTML += `
-        <img class="playing-card" src="./images/cards/${dealerHand[dealerHand.length - 1]}.png" alt="Image of a playing card">
-      `;
-    document.querySelector('.js-dealer-score')
-      .innerHTML = `${calculateHandTotal(dealerHand)}`;
-  } 
+    document.querySelector('.js-dealer-cards').innerHTML += `
+      <img class="playing-card" src="./images/cards/${dealerHand[dealerHand.length - 1]}.png" alt="Image of a playing card">
+    `;
+    document.querySelector('.js-dealer-score').innerHTML = calculateHandTotal(dealerHand);
+  }
+
+  determineWinner(playerHand, dealerHand);
 }
 
-//event listener for the stand button
-document.querySelector('.js-stand-button')
-  .addEventListener('click', function(){
-    stand(deck, dealerHand);
-    determineWinner(playerHand, dealerHand);
-  document.querySelector('.js-result').innerHTML = resultHTML += `${determineWinner(playerHand, dealerHand)}`;
-  });
-
-//function to determine the winner of the game
-function determineWinner(playerHand, dealerHand){
+// Function to determine the winner of the game
+function determineWinner(playerHand, dealerHand) {
   const playerTotal = calculateHandTotal(playerHand);
   const dealerTotal = calculateHandTotal(dealerHand);
 
-  if ((playerTotal === 21 && playerHand.length === 2) && (dealerTotal === 21 && dealerHand.length === 2)) {
-    return 'Push';
-  } else if ((playerTotal === 21 && playerHand.length === 2) && (dealerTotal !== 21 && dealerHand.length !== 2)) {
-    return 'Player Wins';
-  } else if (playerTotal > 21) {
-    return 'You Lose';
-  } else if (dealerTotal > 21) {
-    return 'Player Wins';
-  } else if (playerTotal > dealerTotal) {
-    return 'Player Wins';
+  if (playerTotal > 21) {
+    resultHTML = 'You Lose';
+  } else if (dealerTotal > 21 || playerTotal > dealerTotal) {
+    resultHTML = 'Player Wins';
   } else if (playerTotal < dealerTotal) {
-    return 'Dealer Wins';
+    resultHTML = 'Dealer Wins';
   } else {
-    return 'Push';
+    resultHTML = 'Push';
   }
-  
+
+  document.querySelector('.js-result').innerHTML = resultHTML;
 }
 
-//function to start a new game when the new game button is clicked
-document.querySelector('.js-new-game-button')
-  .addEventListener('click', function(){
-    location.reload();
-  });
+// Event listeners
+document.querySelector('.js-hit-button').addEventListener('click', () => hit(deck, playerHand));
+document.querySelector('.js-stand-button').addEventListener('click', () => stand(deck, dealerHand));
+document.querySelector('.js-new-game-button').addEventListener('click', () => location.reload());
+document.querySelector('.js-strategy-button').addEventListener('click', () => {
+  document.querySelector('.js-strategy-div').classList.toggle('hidden');
+});
 
-
-  document.querySelector('.js-strategy-button')
-  .addEventListener('click', function() {
-    const strategyDiv = document.querySelector('.js-strategy-div');
-    strategyDiv.classList.toggle('hidden');
-  });
+// Initial deal
+dealCards(deck, playerHand, dealerHand);
